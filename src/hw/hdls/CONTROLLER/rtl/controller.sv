5d4c5a452c5ee8e3ebb27d535aff809b38fe389c
hossein1387 <m.h.askari.hemmat@gmail.com>
2018-11-26 17:22:51 -0500

    signal renaming

--- src/hw/hdls/CONTROLLER/rtl/controller.sv
+++ src/hw/hdls/CONTROLLER/rtl/controller.sv
@@ -5,62 +5,61 @@ module controller
         parameter NUM_NEURONS_L1       = 1024,
         parameter NUM_NEURONS_L2       = 64,
         parameter NUM_NEURONS_L3       = 10,
-        parameter WEIGHT_DATA_WIDTH_L1 = 2048,
-        parameter WEIGHT_DATA_WIDTH_L2 = 10,
-        parameter WEIGHT_DATA_WIDTH_L3 = 10,
+        parameter WEIGHT_DATA_WIDTH_L1 = 512,
+        parameter WEIGHT_DATA_WIDTH_L2 = 2048,
+        parameter WEIGHT_DATA_WIDTH_L3 = 128,
         parameter WEIGHT_ADDR_WIDTH_L1 = 10,
         parameter WEIGHT_ADDR_WIDTH_L2 = 10,
         parameter WEIGHT_ADDR_WIDTH_L3 = 10,
-        parameter BIAS_DATA_WIDTH_L1   = 2048,
-        parameter BIAS_DATA_WIDTH_L2   = 10,
-        parameter BIAS_DATA_WIDTH_L3   = 10,
+        parameter BIAS_DATA_WIDTH_L1   = 2,
+        parameter BIAS_DATA_WIDTH_L2   = 2,
+        parameter BIAS_DATA_WIDTH_L3   = 2,
         parameter BIAS_ADDR_WIDTH_L1   = 10,
         parameter BIAS_ADDR_WIDTH_L2   = 10,
-        parameter BIAS_ADDR_WIDTH_L3   = 10,
+        parameter BIAS_ADDR_WIDTH_L3   = 10
     )
     (
         input  logic clk,
         input  logic rst,
         input  logic start,
-        input  logic valid1,
-        input  logic valid2,
+        input  logic valid_1,
+        input  logic valid_2,
 
         // Layer1
         // Weight
-        input logic  [WEIGHT_DATA_WIDTH_L1-1 : 0] weight_data_l1,
-        output logic [WEIGHT_DATA_WIDTH_L1-1 : 0] weight_addr_l1,
-        output logic [WEIGHT_DATA_WIDTH_L1-1 : 0] weight_ren_l1,
+        input  logic [WEIGHT_DATA_WIDTH_L1-1 : 0] weight_data_l1,
+        output logic [WEIGHT_ADDR_WIDTH_L1-1 : 0] weight_addr_l1,
+        output logic                              weight_ren_l1,
         output logic [WEIGHT_DATA_WIDTH_L1-1 : 0] weight_out_l1,
         // Weight
-        input logic  [WEIGHT_DATA_WIDTH_L1-1 : 0] bias_data_l1,
-        output logic [WEIGHT_DATA_WIDTH_L1-1 : 0] bias_addr_l1,
-        output logic [WEIGHT_DATA_WIDTH_L1-1 : 0] bias_ren_l1,
-        output logic [WEIGHT_DATA_WIDTH_L1-1 : 0] bias_out_l1,
+        input  logic   [BIAS_DATA_WIDTH_L1-1 : 0] bias_data_l1,
+        output logic   [BIAS_ADDR_WIDTH_L1-1 : 0] bias_addr_l1,
+        output logic                              bias_ren_l1,
+        output logic   [BIAS_DATA_WIDTH_L1-1 : 0] bias_out_l1,
 
         // Layer2
         // Weight
-        input logic  [WEIGHT_DATA_WIDTH_L2-1 : 0] weight_data_l2,
-        output logic [WEIGHT_DATA_WIDTH_L2-1 : 0] weight_addr_l2,
-        output logic [WEIGHT_DATA_WIDTH_L2-1 : 0] weight_ren_l2,
+        input  logic [WEIGHT_DATA_WIDTH_L2-1 : 0] weight_data_l2,
+        output logic [WEIGHT_ADDR_WIDTH_L2-1 : 0] weight_addr_l2,
+        output logic                              weight_ren_l2,
         output logic [WEIGHT_DATA_WIDTH_L2-1 : 0] weight_out_l2,
         // Weight
-        input logic  [WEIGHT_DATA_WIDTH_L2-1 : 0] bias_data_l2,
-        output logic [WEIGHT_DATA_WIDTH_L2-1 : 0] bias_addr_l2,
-        output logic [WEIGHT_DATA_WIDTH_L2-1 : 0] bias_ren_l2,
-        output logic [WEIGHT_DATA_WIDTH_L2-1 : 0] bias_out_l2,
+        input  logic   [BIAS_DATA_WIDTH_L2-1 : 0] bias_data_l2,
+        output logic   [BIAS_ADDR_WIDTH_L2-1 : 0] bias_addr_l2,
+        output logic                              bias_ren_l2,
+        output logic   [BIAS_DATA_WIDTH_L2-1 : 0] bias_out_l2,
 
         // Layer3
         // Weight
-        input logic  [WEIGHT_DATA_WIDTH_L3-1 : 0] weight_data_l3,
-        output logic [WEIGHT_DATA_WIDTH_L3-1 : 0] weight_addr_l3,
-        output logic [WEIGHT_DATA_WIDTH_L3-1 : 0] weight_ren_l3,
+        input  logic [WEIGHT_DATA_WIDTH_L3-1 : 0] weight_data_l3,
+        output logic [WEIGHT_ADDR_WIDTH_L3-1 : 0] weight_addr_l3,
+        output logic                              weight_ren_l3,
         output logic [WEIGHT_DATA_WIDTH_L3-1 : 0] weight_out_l3,
         // Weight
-        input logic  [WEIGHT_DATA_WIDTH_L3-1 : 0] bias_data_l3,
-        output logic [WEIGHT_DATA_WIDTH_L3-1 : 0] bias_addr_l3,
-        output logic [WEIGHT_DATA_WIDTH_L3-1 : 0] bias_ren_l3,
-        output logic [WEIGHT_DATA_WIDTH_L3-1 : 0] bias_out_l3,
-
+        input  logic   [BIAS_DATA_WIDTH_L3-1 : 0] bias_data_l3,
+        output logic   [BIAS_ADDR_WIDTH_L3-1 : 0] bias_addr_l3,
+        output logic                              bias_ren_l3,
+        output logic   [BIAS_DATA_WIDTH_L3-1 : 0] bias_out_l3
     );
 
 
@@ -72,7 +71,7 @@ module controller
     always_ff @(posedge clk) begin
         if(~rst) begin
             neuron_cnt    <= 0;
-            next_state    <= IDLDE;
+            next_state    <= IDLE;
             bias_ren_l1   <= 1'b0;
             bias_ren_l2   <= 1'b0;
             bias_ren_l3   <= 1'b0;
@@ -95,14 +94,14 @@ module controller
                         weight_ren_l1 <= 1'b1;
                         bias_addr_l1  <= 0;
                         weight_addr_l1<= 0;
-                    end else if (valid1 == 1'b1) begin
+                    end else if (valid_1 == 1'b1) begin
                         next_state    <= CALC_L2;
                         neuron_cnt    <= 0;
                         bias_ren_l2   <= 1'b1;
                         weight_ren_l2 <= 1'b1;
                         bias_addr_l2  <= 0;
                         weight_addr_l2<= 0;
-                    end else if (valid2 == 1'b1) begin
+                    end else if (valid_2 == 1'b1) begin
                         next_state    <= CALC_L3;
                         neuron_cnt    <= 0;
                         bias_ren_l3   <= 1'b1;
